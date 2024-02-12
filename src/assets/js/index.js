d2c7ae11be1989b43cdf906e18ad1b2887f8dd34
OrgeAlexj06 <jovanovicalexandre06@gmail.com>
2024-02-12 14:26:09 +0100

    change downloads directory

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -29,9 +29,9 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n'est pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "Je... vie...", "author": "OrgeAlexj06" },
+            { "message": "Salut je suis du code.", "author": "OrgeAlexj06" },
+            { "message": "Linux n'est pas un os, mais un kernel.", "author": "OrgeAlexj06" }
         ];
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
